90b1beb538ec30452977e719a7e678223006af9a
Evelyn-dara <daraevelyn7@gmail.com>
2024-02-09 12:17:57 +0100

    first commit

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams, useSearchParams } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+// User Profile
+const UserProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get('name');
+
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+
+      setUserPosts(data);
+    };
+
+    if (params?.id) fetchPosts();
+  }, [params?.id]);
+
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      data={userPosts}
+    />
+  );
+};
+
+export default UserProfile;
