5d48d9e5fde43fb543964b7616a259680a0fea74
Evelyn-dara <daraevelyn7@gmail.com>
2024-02-16 18:28:57 +0100

    ...

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -7,7 +7,7 @@ import Form from "@components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
-   const searchParams = new URLSearchParams(window.location.search);
+   const searchParams = new URLSearchParams();
     const promptId = searchParams.get("id");
 
   const [post, setPost] = useState({ prompt: "", tag: "", });
