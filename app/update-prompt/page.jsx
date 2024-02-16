abaa3e7bd98febbe1acf7299e449fd20c14faed7
Evelyn-dara <daraevelyn7@gmail.com>
2024-02-16 09:56:27 +0100

    ...

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, Suspense} from "react";
+import { useEffect, useState} from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -53,7 +53,7 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <Suspense>
+    
     <Form
       type='Edit'
       post={post}
@@ -61,7 +61,7 @@ const UpdatePrompt = () => {
       submitting={submitting}
       handleSubmit={updatePrompt}
     />
-    </Suspense>
+   
   );
 };
 
